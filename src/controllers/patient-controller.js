c3d9087a43961308c7c58c2e549efd1cd4e28823
Shreyansh <patelshreyansh376@gmail.com>
2025-01-27 17:29:18 +0530

    created getPatients and getPatientById

--- src/controllers/patient-controller.js
+++ src/controllers/patient-controller.js
@@ -1,6 +1,6 @@
 const { StatusCodes } = require("http-status-codes")
 const db = require("../models")
-const { ValidationError } = require("sequelize")
+const { ValidationError, where } = require("sequelize")
 
 async function createPatient(req,res) { 
     try {
@@ -37,4 +37,47 @@ async function createPatient(req,res) {
     
 }
 
-module.exports={createPatient}
+async function getAllPatients(req,res) {
+    try {
+        const patients=await db.Patients.findAll()
+        return res.status(StatusCodes.OK).json(patients)
+    } catch (error) {
+        if (error instanceof ValidationError) {
+            // Handle validation errors
+            console.error('Validation Errors:');
+            error.errors.forEach((err) => {
+              console.error(`${err.path}: ${err.message}`);
+            });
+          } else {
+            console.error('Error: while', error);
+          }
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Error while getting all patients")
+    }
+}
+async function getPatientById(req,res){
+    try {
+        const {id}=req.params
+        const patient=await db.Patients.findOne({
+            where:{id}
+        })
+        if(!patient){
+            return res.status(StatusCodes.BAD_REQUEST).json("patient with this id does not exist")
+        }
+        return res.status(StatusCodes.OK).json(patient)
+    } catch (error) {
+        if (error instanceof ValidationError) {
+            // Handle validation errors
+            console.error('Validation Errors:');
+            error.errors.forEach((err) => {
+              console.error(`${err.path}: ${err.message}`);
+            });
+          } else {
+            console.error('Error: while', error);
+          }
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).send("Error while getting atient by id")
+    }
+}
+async function updatePatientById(req,res){
+    
+}
+module.exports={createPatient,getAllPatients,getPatientById}
--- src/routes/v1/patient.js
+++ src/routes/v1/patient.js
@@ -6,4 +6,6 @@ const { patientController } = require("../../controllers");
 const router = express.Router();
 
 router.post('/patient',verifyToken,patientController.createPatient)
+router.get('/patient',verifyToken,patientController.getAllPatients)
+router.get('/patient/:id',verifyToken,patientController.getPatientById)
 module.exports = router;
